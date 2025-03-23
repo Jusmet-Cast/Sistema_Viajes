8e8124d7e7d6817aeeb30d7e63eff4d12dded14b
Justyn A. Castro <ahmetcast@gmail.com>
2025-03-22 18:32:28 -0600

    Implementación de función para menu principal

--- ui.py
+++ ui.py
@@ -50,3 +50,61 @@ def mostrar_login():
     salir_button.grid(row=3, column=0, columnspan=2, pady=10)
 
     ventana.mainloop()
+
+# Implementa la interfaz del menu principal.
+def mostrar_menu_principal(usuario):
+    ventana_menu = tk.Tk()
+    ventana_menu.title("Menú Principal")
+
+    # Centrar la ventana en la pantalla.
+    ancho_ventana = 300
+    alto_ventana = 250  # Aumentamos la altura para acomodar más espacio.
+    x_pos = (ventana_menu.winfo_screenwidth() // 2) - (ancho_ventana // 2)
+    y_pos = (ventana_menu.winfo_screenheight() // 2) - (alto_ventana // 2)
+    ventana_menu.geometry(f"{ancho_ventana}x{alto_ventana}+{x_pos}+{y_pos}")
+
+    # Mostrar información del usuario.
+    tk.Label(ventana_menu, text=f"Bienvenido, {usuario[1]} ").pack(pady=10)
+
+    # Botones para las opciones del menú.
+    if usuario[4] == "Gerente de tienda":   # Validación de usuario con rol/perfil "Gerente de tienda".
+        tk.Button(ventana_menu, text="Registrar Viaje", command=lambda: print("Registrar Viaje")).pack(pady=5)
+        tk.Button(ventana_menu, text="Asignar Sucursal", command=lambda: print("Asignar Sucursal")).pack(pady=5)
+        tk.Button(ventana_menu, text="Generar Reportes", command=lambda: print("Generar Reportes")).pack(pady=5)
+    else:   # Opciones para otros usuarios.
+        tk.Button(ventana_menu, text="Asignar Sucursal", command=lambda: print("Asignar Sucursal")).pack(pady=5)
+        tk.Button(ventana_menu, text="Generar Reportes", command=lambda: print("Generar Reportes")).pack(pady=5)
+
+    tk.Label(ventana_menu, text="").pack(pady=10)  # Etiqueta vacía para crear espacio (estética).
+
+    # Frame para agrupar los botones de salida.
+    frame_botones_salida = tk.Frame(ventana_menu)
+    frame_botones_salida.pack(pady=10)
+
+    # Función para regresar al login.
+    def regresar_al_login():
+        ventana_menu.destroy()  # Cierra la ventana actual.
+        mostrar_login()         # Abre la ventana de login.
+
+    def salir():
+        ventana_menu.destroy()
+
+    # Botón para regresar al login.
+    tk.Button(
+        frame_botones_salida,
+        text="Regresar al Login",
+        command=regresar_al_login,  # Llama a la función para regresar al login.
+        bg="yellow",  # Fondo amarillo.
+        fg="black"    # Texto negro.
+    ).pack(side=tk.LEFT, padx=10)  # Alineación horizontal con espacio entre botones.
+
+    # Botón de salir.
+    tk.Button(
+        frame_botones_salida,
+        text="Salir",
+        command=salir,  # Cierra la aplicación.
+        bg="red",     # Fondo rojo.
+        fg="white"    # Texto blanco.
+    ).pack(side=tk.LEFT, padx=10)  # Alineación horizontal con espacio entre botones.
+
+    ventana_menu.mainloop()
